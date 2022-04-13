b61170be35030d9427fab3f90cee6375484883e5
Mateu57 <76662029+Mateu57@users.noreply.github.com>
2022-04-13 19:27:17 +0200

    Delete Flappy directory

--- Flappy/Buttons/FxButton.gd
+++
@@ -1,6 +0,0 @@
-extends TextureButton
-
-var music_bus_fx = AudioServer.get_bus_index("Effects")
-
-func _on_FxButton_pressed():
-	AudioServer.set_bus_mute(music_bus_fx, not AudioServer.is_bus_mute(music_bus_fx))
